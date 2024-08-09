504f3069fde8602033de578c9c92e15c4296be47
sangam5756 <sangammunde3@gmail.com>
2024-08-10 01:19:41 +0530

    revision

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-// function statement
-
-function a() {
-  console.log("a called");
-}
-
-a();
-
-//function expression
-var b = function (param1, param2) {
-  console.log("b called");
-};
-
-
-// anonymous function  without the name
-// function (){
-
-// }
-
-// named function expression
-let c  = function xyz(){
-  console.log("xys")
-
-  
-}
-
-c();
-
-
+// callback function
 
 
